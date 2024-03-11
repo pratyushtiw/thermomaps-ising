185146f1365ec75ced8539fb4d4c0c0332996c45
pratyushtiw <30470921+pratyushtiw@users.noreply.github.com>
2024-03-11 15:24:05 -0400

    Update base.py
    betaJx betaJy variables defined

--- thermomaps-root/ising/base.py
+++ thermomaps-root/ising/base.py
@@ -16,7 +16,7 @@ logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 class IsingModel:
-    def __init__(self, sampler: Type['Sampler'], size: int, warmup: int, temp: float, Jx: float = 1.0, Jy: float = 1.0):
+    def __init__(self, sampler: Type['Sampler'], size: int, warmup: int, temp: float, Jx: float = 1.0, Jy: float = 1.0, betaJx: float = 1.0, betaJy: float = 1.0):
         """
         Initialize the base Ising model.
 
