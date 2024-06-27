6d78ced10464f8eba402dd431f790cc69fbfac1a
pratyushtiw <30470921+pratyushtiw@users.noreply.github.com>
2024-06-27 07:45:36 -0400

    Update base.py
    Teff = 2*(Jx/temp +  2)

--- thermomaps-root/ising/base.py
+++ thermomaps-root/ising/base.py
@@ -39,8 +39,8 @@ class IsingModel:
         sampler = sampler(self)
         self.update = sampler.update
         self.trajectory = EnsembleIsingTrajectory(
-            summary = Summary(name="IsingModel", size=size, temperature=temp, Jx=Jx, Jy=Jy, ToverJx=temp/Jx, ToverJy=temp/Jy, Teff = Jx/temp +  2,   sampler=sampler.name),
-            state_variables = Summary(temperature=temp, Jx=Jx, Jy=Jy, ToverJx=temp/Jx, ToverJy=temp/Jy, Teff = Jx/temp +  2)
+            summary = Summary(name="IsingModel", size=size, temperature=temp, Jx=Jx, Jy=Jy, ToverJx=temp/Jx, ToverJy=temp/Jy, Teff = 2*(Jx/temp +  2),   sampler=sampler.name),
+            state_variables = Summary(temperature=temp, Jx=Jx, Jy=Jy, ToverJx=temp/Jx, ToverJy=temp/Jy, Teff = 2*(Jx/temp +  2))
 
         
             # state_variables = Summary(temperature=temp)
